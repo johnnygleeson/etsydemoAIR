ffcd30403efce23bdcb4d61361e47f3da71f01a5
John Gleeson <johnnygleeson@gmail.com>
2015-09-07 10:41:28 +0100

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Etsydemo::Application.application.configure do
+Etsydemo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
